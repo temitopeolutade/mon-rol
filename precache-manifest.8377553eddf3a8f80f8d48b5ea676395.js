4a3e7c5b7e165b27833027daf63b79acb44a38a1
temitopeolutade <topeolutade@yahoo.com>
2020-04-02 22:18:53 +0100

    Updates

---
+++ precache-manifest.8377553eddf3a8f80f8d48b5ea676395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f508e146c900a6b745a99c5aa0dc48f",
+    "url": "/mon-rol/index.html"
+  },
+  {
+    "revision": "53f4953420d7fcea87c8",
+    "url": "/mon-rol/static/css/main.a0744f62.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mon-rol/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mon-rol/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f4953420d7fcea87c8",
+    "url": "/mon-rol/static/js/main.767d3d9b.chunk.js"
+  },
+  {
+    "revision": "d967577eb807b6425e8a",
+    "url": "/mon-rol/static/js/runtime-main.fc0dd02b.js"
+  }
+]);
