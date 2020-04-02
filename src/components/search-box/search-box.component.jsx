eedf3e7c97af1615e931733ea266a709fe989220
temitopeolutade <topeolutade@yahoo.com>
2020-04-02 17:13:16 +0100

    adding files

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './search-box.styles.css'
+
+export const SearchBox = ({placeholder,handleChange}) => (
+
+    <input
+
+    className ='search'
+    
+    type='search' 
+    
+    placeholder={placeholder}
+    
+    onChange={handleChange}
+   
+        />
+
+)
